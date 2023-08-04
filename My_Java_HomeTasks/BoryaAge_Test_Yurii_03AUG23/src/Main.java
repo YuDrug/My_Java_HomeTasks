3e1e1866bf0c4c28c68d1eb277edd0ba5f84e2bd
YuDrug <141164135+YuDrug@users.noreply.github.com>
2023-08-04 20:18:00 +0300

    My Java Projects Tasks and Drafts

---
+++ My_Java_HomeTasks/BoryaAge_Test_Yurii_03AUG23/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int boriaAge = 15;
+        System.out.println("БОРИ возраст - " + boriaAge + " лет");
+    }
+}
+//Ой, я ошибся! Боре всего 14. Уменьшите возраст Бори в переменной на 1 и выведите новое сообщение с возрастом
